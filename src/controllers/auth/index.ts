b3c09f432de397400a9dda48f5af503a824901df
juancarlos301 <juancarlos.31.2002@gmail.com>
2023-12-19 17:43:29 -0500

    add login

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,1 +1,2 @@
-export { createUser } from "./create";
+export { createUser } from './create';
+export { logIn } from './login';
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,1 +1,2 @@
-export { createUser } from "./auth";
+export { createUser } from './auth';
+export { logIn } from './auth';
